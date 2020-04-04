2200b9fb54b25ded9dba9bbc22964ad9f8d06e6e
samuelgerges <gergess@sheridancollege.ca>
2020-04-03 21:11:20 -0400

    Initial commit

---
+++ src/test/java/sheridan/gergess/quiz2/Quiz2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.gergess.quiz2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
